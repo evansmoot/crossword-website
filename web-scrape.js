38ff99a3dbde83b1d373ee231063d88a37044444
evansmoot <smootev@msn.com>
2018-05-21 23:36:08 -0400

    Added url to pull from and testing github credential storer

--- web-scrape.js
+++ web-scrape.js
@@ -4,6 +4,11 @@ const request = require('request');
 const cheerio = require('cheerio');
 const app = express();
 
+app.get('/scrape', function(req, res) {
+    url = 'https://www.laxcrossword.com/';
+    
+});
+
 app.listen('8080');
 console.log('Server is listening on port 8080');
 
