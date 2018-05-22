b15ffc16e54bc456529b2997079f7382044044b2
evansmoot <smootev@msn.com>
2018-05-21 23:09:25 -0400

    Created package.json file and required imports in JS file

---
+++ web-scrape.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const fs = require('fs');
+const request = require('request');
+const cheerio = require('cheerio');
+const app = express();
+
+module.exports = app;
