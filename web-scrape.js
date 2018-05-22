cf09554fcda735859e29fc8689966bdee167a4b9
evansmoot <smootev@msn.com>
2018-05-21 23:30:54 -0400

    Created server on port and added gitignore

--- web-scrape.js
+++ web-scrape.js
@@ -4,4 +4,7 @@ const request = require('request');
 const cheerio = require('cheerio');
 const app = express();
 
+app.listen('8080');
+console.log('Server is listening on port 8080');
+
 module.exports = app;
