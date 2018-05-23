5e0639b11402ddfede0abe58bcc9ddfe7481da5f
evansmoot <smootev@msn.com>
2018-05-22 22:05:31 -0400

    Pulled data from site and parsed into non-pretty array

--- web-scrape.js
+++ web-scrape.js
@@ -4,13 +4,26 @@ const request = require('request');
 const cheerio = require('cheerio');
 const app = express();
 
-app.get('/scrape', function(req, res) {
-    url = 'https://www.laxcrossword.com/';
+const baseUrl = 'https://www.laxcrossword.com/';
 
-    request(url, function(error, response, html) {
-        
-    });
+
+request(baseUrl, function(error, response, html) {
+    if (!error) {
+        var $ = cheerio.load(html);
+        $("#clue_list").filter(function (){
+            var rawData = $(this).children().eq(2).text() + "\n" + $(this).children().eq(4).text().trim();
+            var clues = rawData.split(/\r?\n/); //splits scraped data into array based on carriage returns
+            clues = clues.map(function(elem) { //removes numbers of clues from array
+                return elem.replace(/\d{1,2}\. /, "");
+            });
+            //TODO: Remove spaces and punctuation from clue answers
+            console.log(clues);
+        })
+    } else {
+        console.log("Error Encountered: " + error);
+    }
 });
+
 
 app.listen('8080');
 console.log('Server is listening on port 8080');
